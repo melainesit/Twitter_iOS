073abe8180a5c82fd07510cc629b5c405fd7f579
MelaineSit <melaineist@gmail.com>
2021-03-06 20:57:57 -0500

    Fix comments

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -13,7 +13,6 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -37,14 +36,6 @@ class LoginViewController: UIViewController {
         })
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
